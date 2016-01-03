2080946215c764e8b2ff917c1939d856463baf11
Jim Strother <strotherwebdev@gmail.com>
2016-01-03 03:59:57 +0000

    Clear Fields button working correctly

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-        $('#itemForm').submit(function(e) {
+    $('#item').focus();
+    $('#itemForm').submit(function(e) {
         e.preventDefault();
         // declared variables
         var counter = $('#displayList tbody').children().length;
@@ -7,7 +8,8 @@ $(document).ready(function() {
         var quantity = $('#quantity').val();
         var cost = $('#cost').val();
         var done = $('<td><input type=\"checkbox\" id=\"done\" name=\"done\" value=\"Done\"></td>');
-        var tr = $('<tr id=' + counter + '>').append($('<td>').text(item)).append($('<td>').text(quantity)).append($('<td>').text(cost)).append(done);
+        var remove = $('<td><input type=\"checkbox\" id=\"delete\" name=\"delete\" value=\"Delete\"></td>');
+        var tr = $('<tr id=' + counter + '>').append($('<td>').text(item)).append($('<td>').text(quantity)).append($('<td>').text(cost)).append(done).append(remove);
         var fixHelper = function(e, ui) {
             ui.children().each(function() {
                 $(this).width($(this).width());
@@ -24,24 +26,23 @@ $(document).ready(function() {
                 ui.draggable;
             }
         });
-        // resetting input field values
-        $('#item').val('');
+        // clearing input field values
+        $('#item').val('').focus();
         $('#quantity').val('');
         $('#cost').val('');
     });
-    // so, this doesn't work.  try again
-    $('#itemForm').reset(function(e) {
+    $('#reset').click(function(e) {
         e.preventDefault();
-        // resetting input field values
+        // clearing input field values
         $('#item').val('');
         $('#quantity').val('');
-        $('#cost').val('');       
+        $('#cost').val('');
     });
     checkDone();
 });
 // I think I'm on the right track with this.  Trying to filter the <tr> to the <tfooter> to get it out of the main list. If unchecked, it will go back to <tbody>.
 function checkDone() {
-    if ($('#done').checked) {
-        console.log('checked!');
+    if ($('#done').prop('checked)')) {
+        console.log('hurrah!');
     }
 }
