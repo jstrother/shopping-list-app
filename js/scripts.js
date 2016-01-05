6fe7e5b4c739affed346e62e4d21abfe609123b5
Jim Strother <strotherwebdev@gmail.com>
2016-01-05 01:11:55 +0000

    minor js edits

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,8 @@ $(document).ready(function() {
         var item = $('#item').val();
         var quantity = $('#quantity').val();
         var cost = $('#cost').val();
-        var done = $('<td><input type=\"checkbox\" id=\"done\" name=\"done\" value=\"Done\"></td>');
-        var remove = $('<td><input type=\"checkbox\" id=\"delete\" name=\"delete\" value=\"Delete\"></td>');
+        var done = $('<td><input type=\"checkbox\" class=\"done\" name=\"done\" value=\"Done\"></td>');
+        var remove = $('<td><input type=\"checkbox\" class=\"delete\" name=\"delete\" value=\"Delete\"></td>');
         var tr = $('<tr id=' + counter + '>').append($('<td>').text(item)).append($('<td>').text(quantity)).append($('<td>').text(cost)).append(done).append(remove);
         var fixHelper = function(e, ui) {
             ui.children().each(function() {
@@ -39,18 +39,16 @@ $(document).ready(function() {
         $('#cost').val('');
     });
     // moves item to <tfoot> when marked as done, returns to main list when unchecked
-    $('#markDone').click(function(e) {
-        e.preventDefault();
-        $('#done').change(function() {
-            if (this.checked) {
-                $('tfoot').append($(this).parentsUntil('tbody')); // so far, this is doing something, but not what I want.  it is creating a new row, but it doesn't seem to be in <tfoot> and is empty of values.  the original stays the same in <tbody>
-            }
-        });
+    $('#markDone').on('click', function() {
+        // e.preventDefault();
+        if ($('.done').checked) {
+            $('tfoot').append($('.done').parentsUntil('tbody').children().val()); // so far, this is doing something, but not what I want.  it is creating a new row, but it doesn't seem to be in <tfoot> and is empty of values.  the original stays the same in <tbody>
+        }
     });
     // trying to delete item from list completely, so far something is happening, but it is the same as markDone above
-    $('#remove').click(function(e) {
-        e.preventDefault();
-        if (this.checked) {
+    $('#remove').on('click', function() {
+        // e.preventDefault();
+        if ($('.delete').checked) {
             $(this).parentsUntil('tbody').remove();
         }
     });
